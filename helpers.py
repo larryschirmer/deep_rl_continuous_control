b4c0f1d75c583bd17cb03c278236e19f4406c543
Larry Schirmer <larryschirmer@gmail.com>
2019-08-13 22:11:42 -0400

    use the max predicted value for all four models

--- helpers.py
+++ helpers.py
@@ -121,9 +121,6 @@ def run_episode(model, optimizers, params, epoch, train):
     values, logprobs, rewards = [], [], []
     done = False
 
-    loss, actor_loss, critic_loss = (
-        torch.tensor(0), torch.tensor(0), torch.tensor(0))
-
     step_count = 0
     while (done == False):
         step_count += 1
@@ -131,6 +128,8 @@ def run_episode(model, optimizers, params, epoch, train):
         policies1_dist, value1 = model[1](state, epoch)
         policies2_dist, value2 = model[2](state, epoch)
         policies3_dist, value3 = model[3](state, epoch)
+
+        value = torch.max(torch.tensor([value0, value1, value2, value3]))
 
         action0 = torch.clamp(policies0_dist.rsample(), min=-1, max=1)
         action1 = torch.clamp(policies1_dist.rsample(), min=-1, max=1)
@@ -141,7 +140,7 @@ def run_episode(model, optimizers, params, epoch, train):
         logprob2 = policies2_dist.log_prob(action2)
         logprob3 = policies3_dist.log_prob(action3)
 
-        values.append([value0, value1, value2, value3])
+        values.append(value)
         logprobs.append([logprob0, logprob1, logprob2, logprob3])
 
         action_list = np.array([action0.detach().numpy(), action1.detach(
@@ -178,20 +177,12 @@ def update_params(optimizers, values, logprobs, rewards, params):
     logprob2 = [a[2] for a in logprobs]
     logprob3 = [a[3] for a in logprobs]
 
-    values0 = [v[0] for v in values]
-    values1 = [v[1] for v in values]
-    values2 = [v[2] for v in values]
-    values3 = [v[3] for v in values]
-
+    values = torch.stack(values).flip(dims=(0,)).view(-1)
     rewards = torch.Tensor(rewards).flip(dims=(0,)).view(-1)
     logprob0 = torch.stack(logprob0).flip(dims=(0,)).view(-1)
     logprob1 = torch.stack(logprob1).flip(dims=(0,)).view(-1)
     logprob2 = torch.stack(logprob2).flip(dims=(0,)).view(-1)
     logprob3 = torch.stack(logprob3).flip(dims=(0,)).view(-1)
-    values0 = torch.stack(values0).flip(dims=(0,)).view(-1)
-    values1 = torch.stack(values1).flip(dims=(0,)).view(-1)
-    values2 = torch.stack(values2).flip(dims=(0,)).view(-1)
-    values3 = torch.stack(values3).flip(dims=(0,)).view(-1)
 
     Returns = []
     total_return = torch.Tensor([0])
@@ -214,25 +205,25 @@ def update_params(optimizers, values, logprobs, rewards, params):
 
     ppo_ratio0 = (logprob0 - prev_logprobs0[-1:]).exp()
     torch.cat((prev_logprobs0[1:], logprob0))
-    advantage0 = Returns - values0.detach()
+    advantage0 = Returns - values.detach()
     surrogate00 = ppo_ratio0 * advantage0
     surrogate01 = torch.clamp(ppo_ratio0, 1.0 - params['ppo_epsilon'], 1.0 + params['ppo_epsilon']) * advantage0
 
     ppo_ratio1 = (logprob1 - prev_logprobs1[-1:]).exp()
     torch.cat((prev_logprobs1[1:], logprob1))
-    advantage1 = Returns - values1.detach()
+    advantage1 = Returns - values.detach()
     surrogate10 = ppo_ratio1 * advantage1
     surrogate11 = torch.clamp(ppo_ratio1, 1.0 - params['ppo_epsilon'], 1.0 + params['ppo_epsilon']) * advantage1
 
     ppo_ratio2 = (logprob2 - prev_logprobs2[-1:]).exp()
     torch.cat((prev_logprobs2[1:], logprob2))
-    advantage2 = Returns - values2.detach()
+    advantage2 = Returns - values.detach()
     surrogate20 = ppo_ratio2 * advantage2
     surrogate21 = torch.clamp(ppo_ratio2, 1.0 - params['ppo_epsilon'], 1.0 + params['ppo_epsilon']) * advantage2
 
     ppo_ratio3 = (logprob3 - prev_logprobs3[-1:]).exp()
     torch.cat((prev_logprobs3[1:], logprob3))
-    advantage3 = Returns - values3.detach()
+    advantage3 = Returns - values.detach()
     surrogate30 = ppo_ratio3 * advantage3
     surrogate31 = torch.clamp(ppo_ratio3, 1.0 - params['ppo_epsilon'], 1.0 + params['ppo_epsilon']) * advantage3
 
@@ -241,10 +232,10 @@ def update_params(optimizers, values, logprobs, rewards, params):
     actor_loss2 = - torch.min(surrogate20, surrogate21) * gae_reduction
     actor_loss3 = - torch.min(surrogate30, surrogate31) * gae_reduction
 
-    critic_loss0 = torch.pow(values0 - Returns, 2)
-    critic_loss1 = torch.pow(values1 - Returns, 2)
-    critic_loss2 = torch.pow(values2 - Returns, 2)
-    critic_loss3 = torch.pow(values3 - Returns, 2)
+    critic_loss0 = torch.pow(values - Returns, 2)
+    critic_loss1 = torch.pow(values - Returns, 2)
+    critic_loss2 = torch.pow(values - Returns, 2)
+    critic_loss3 = torch.pow(values - Returns, 2)
 
     actor_loss0 = torch.clamp(actor_loss0.sum(), min=-params['gradient_clip'], max=params['gradient_clip'])
     actor_loss1 = torch.clamp(actor_loss1.sum(), min=-params['gradient_clip'], max=params['gradient_clip'])
